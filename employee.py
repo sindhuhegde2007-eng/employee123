7090777f7474b9c6e8366c6f89326723d7dabf0f
sindhuhegde2007-eng <sindhuhegde2007@gmail.com>
2025-11-19 10:56:29 +0530

    save

---
+++ employee.py
@@ -0,0 +1,28 @@
+import sys
+if len(sys.argv) != 5:
+    print("Usage: python bonus_calc.py <employee_name> <performance_rating> <performance_bonus> <bonus_percentage>")
+    
+    employee_name = "sara"
+    performance_rating = 2.5
+    performance_bonus = 12
+    bonus_percentage = 5   
+else:
+    employee_name = sys.argv[1]
+    performance_rating = float(sys.argv[2])
+    performance_bonus = float(sys.argv[3])
+    bonus_percentage = float(sys.argv[4])
+
+if performance_rating >= 3.5:
+    salary = 60000
+elif performance_rating >= 2.5:
+    salary = 40000
+else:
+    salary = 15000
+
+annual_bonus = salary * (bonus_percentage / 100)
+
+print("Employee Name:", employee_name)
+print("Performance Rating:", performance_rating)
+print("Performance Bonus:", performance_bonus)
+print("Bonus Percentage:", bonus_percentage)
+print("Annual Bonus:", annual_bonus)
